19cf241de0133b6c01cb729984433625ece89011
achillmakkar <fhb221268@fh-vie.ac.at>
2024-05-07 19:47:50 +0200

    Req "Statistics as PDF": Modified some small things like color, orientation of x-axis, and other things.

--- TouristOffice-Spring/src/main/java/at/ac/fhvie/s24/swpj4bb/touristoffice/demo/business/service/ReportService.java
+++ TouristOffice-Spring/src/main/java/at/ac/fhvie/s24/swpj4bb/touristoffice/demo/business/service/ReportService.java
@@ -9,15 +9,23 @@ import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.CategoryAxis;
+import org.jfree.chart.plot.CategoryPlot;
 import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.renderer.category.BarRenderer;
 import org.jfree.data.category.DefaultCategoryDataset;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
+import org.jfree.chart.axis.CategoryLabelPositions;
 
+
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import java.util.*;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -104,7 +112,32 @@ public class ReportService {
             dataset.addValue(data.getTotalUsedRooms(), "Sum of all Hotel Room Occupancies", data.getPeriod());
             dataset.addValue(data.getTotalUsedBeds(), "Sum of all Hotel Bed Occupancies", data.getPeriod());
         }
-        return ChartFactory.createBarChart("Occupancy Histogram", "Period", "Count", dataset, PlotOrientation.VERTICAL, true, true, false);
+
+        JFreeChart chart = ChartFactory.createBarChart(
+                "Occupancy Histogram", // Chart title
+                "Period", // Domain axis label
+                "Count", // Range axis label
+                dataset, // Data
+                PlotOrientation.VERTICAL,
+                true, // Include legend
+                true, // Tooltips
+                false // URLs
+        );
+
+
+        CategoryPlot plot = chart.getCategoryPlot();
+        CategoryAxis xAxis = plot.getDomainAxis();
+
+        // Labels drehen, um mehr Platz zu schaffen
+        xAxis.setCategoryLabelPositions(CategoryLabelPositions.createUpRotationLabelPositions(Math.PI / 4.0));
+
+        // Schriftgröße anpassen
+        xAxis.setTickLabelFont(new Font("SansSerif", Font.PLAIN, 10));
+        BarRenderer renderer = (BarRenderer) plot.getRenderer();
+        renderer.setSeriesPaint(0, new Color(255, 189, 89));
+        renderer.setSeriesPaint(1, new Color(74, 141, 208));
+
+        return chart;
     }
 
     private BufferedImage getChartImage(JFreeChart chart) {
